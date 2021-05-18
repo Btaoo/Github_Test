6c948c5b9d00e95bfe7dc4235a4291cc8329c085
Btaoo <1337495971@qq.com>
2021-05-18 15:29:22 +0800

    fix(test/Test): 删除了输入语句111

--- src/com/zwt/test/Test.java
+++ src/com/zwt/test/Test.java
@@ -4,6 +4,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!!");
         System.out.println("Hello Wn");
-        System.out.println("123");
     }
 }
