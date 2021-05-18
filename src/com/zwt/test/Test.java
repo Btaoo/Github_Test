95924bdfdd3ce03b1349739c318d2b99bc12f022
Btaoo <1337495971@qq.com>
2021-05-18 15:17:24 +0800

    feat(test/Test): 增加了一个输出功能

--- src/com/zwt/test/Test.java
+++ src/com/zwt/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!!");
         System.out.println("Hello Wn");
+        System.out.println("123");
     }
 }
