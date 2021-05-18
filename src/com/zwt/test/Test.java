c6350f3610b2e6f6bcbb777e05af83afdc742c88
Btaoo <1337495971@qq.com>
2021-05-18 15:06:25 +0800

    Change Test

--- src/com/zwt/test/Test.java
+++ src/com/zwt/test/Test.java
@@ -3,5 +3,6 @@ package com.zwt.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!!");
+        System.out.println("Hello Wn");
     }
 }
