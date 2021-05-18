d7eabd2bf4c40f1ff255458cc0575d9d764163fa
Btaoo <1337495971@qq.com>
2021-05-18 14:16:31 +0800

    Change Student

--- src/com/zwt/pojo/Student.java
+++ src/com/zwt/pojo/Student.java
@@ -3,6 +3,15 @@ package com.zwt.pojo;
 public class Student {
     private String name;
     private String sno;
+    private String sex;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 
     public String getName() {
         return name;
